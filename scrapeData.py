347ea4a9e7843364892810a38822c51e3d681bda
UttU28 <utsavmaan28@gmail.com>
2024-07-23 19:59:37 -0500

    Added Data Filtering

--- scrapeData.py
+++ scrapeData.py
@@ -15,27 +15,48 @@ import schedule
 from dataScraping.jobDescription import *
 from dataScraping.dataHandling import *
 
+contentOut = ["security clearance", "security-clearance", "8+", "9+", "10+", "11+", "12+"]
+contentIn = ["devops", "pipeline", "pipelines", "azure", "aws", "cloud", "cloud engineer", "cloud developer", "terraform", "ansible", "cicd", "ci-ci", "ci/cd", "kubernetes", "flask", "django", "FastAPI", "ETL"]
+
 
 def scrapeTheJobs():
+    def checkRequirementMatching(taroText, shouldBe, shouldNot):
+        for temp1 in shouldBe:
+            if temp1 in taroText:
+                for temp2 in shouldNot:
+                    if temp2 in taroText:
+                        return False
+                return True
+        return False
+
     def writeTheJob(jobID, title, location, company):
+        rawFilePath = 'rawData.json'
+        if os.path.exists(rawFilePath):
+            with open(rawFilePath, 'r', encoding='utf-8') as jsonFile:
+                jobsData = json.load(jsonFile)
+        else: rawData = {}
+
         jsonFilePath = 'jobData.json'
         if os.path.exists(jsonFilePath):
             with open(jsonFilePath, 'r', encoding='utf-8') as jsonFile:
                 jobsData = json.load(jsonFile)
-        else:
-            jobsData = {}
-        if jobID not in jobsData:
+        else: jobsData = {}
+
+        if jobID not in rawData:
             jobsData[jobID] = int(datetime.now(timezone.utc).timestamp())
             description, datePosted, dateUpdated = getJobDescription(jobID)
-            addNewJobSQL(jobID, title, location, company, description, datePosted, dateUpdated)
-            with open(jsonFilePath, 'w', encoding='utf-8') as jsonFile:
-                json.dump(jobsData, jsonFile, ensure_ascii=False, indent=4)
+            checkRequirements = checkRequirementMatching(description, contentIn, contentOut)
+            if checkRequirements:
+                addNewJobSQL(jobID, title, location, company, description, datePosted, dateUpdated)
+                with open(jsonFilePath, 'w', encoding='utf-8') as jsonFile: json.dump(jobsData, jsonFile, ensure_ascii=False, indent=4)
+                with open(rawFilePath, 'w', encoding='utf-8') as jsonFile: json.dump(jobsData, jsonFile, ensure_ascii=False, indent=4)
 
     try:
         chrome_driver_path = 'C:/chromeDriver'
 
         options = Options()
         options.headless = True
+        options.add_argument("--headless")
         options.add_argument("--disable-notifications")
         options.add_argument("--incognito")
         options.add_argument("--disable-popup-blocking")  # Disable popup blocking
