89a40f2604eaa7f6c123d5c1145bba52f064edf6
UttU28 <utsavmaan28@gmail.com>
2024-07-29 16:47:48 -0500

    JSK

--- backend.tf
+++ backend.tf
@@ -5,12 +5,12 @@ terraform {
       version = "=3.0.0"
     }
   }
-  backend "azurerm" {
-    resource_group_name  = "infra-rg"
-    storage_account_name = "clinicainfra"
-    container_name       = "tfstate"
-    key                  = "tfstatealter"
-  }
+  # backend "azurerm" {
+  #   resource_group_name  = "infra-rg"
+  #   storage_account_name = "clinicainfra"
+  #   container_name       = "tfstate"
+  #   key                  = "tfstatealter"
+  # }
 }
 
 provider "azurerm" {
