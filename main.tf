945562bf2c49499fe287a6712c31060fd0962702
UttU28 <utsavmaan28@gmail.com>
2024-07-29 21:15:27 -0500

    JSK

--- main.tf
+++ main.tf
@@ -1,3 +1,17 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+      version = "3.105.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  # Configuration options
+  features {}
+}
+
 resource "azurerm_resource_group" "rg" {
   name     = "rg-containerapps-terraform"
   location = "swedencentral"
