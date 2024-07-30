cc044acca693eb231eb3100a93bb6289cf91996b
UttU28 <utsavmaan28@gmail.com>
2024-07-29 21:12:10 -0500

    JSK

--- main.tf
+++ main.tf
@@ -1,55 +1,105 @@
-terraform {
-  required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "=3.0.0"
-    }
-  }
-  backend "azurerm" {
-    resource_group_name  = "thisstoragerg"
-    storage_account_name = "dicestorage02"
-    container_name       = "13form"
-    key                  = "tfstatedice"
-  }
+resource "azurerm_resource_group" "rg" {
+  name     = "rg-containerapps-terraform"
+  location = "swedencentral"
 }
 
-provider "azurerm" {
-  features {}
+resource "azurerm_log_analytics_workspace" "workspace" {
+  name                = "workspace-aca"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  sku                 = "B1"
+  retention_in_days   = 30
 }
 
-resource "azurerm_resource_group" "backend-app" {
-  name     = "${var.resource_group_name_prefix}-backend"
-  location = var.resource_group_location
-}
-resource "azurerm_service_plan" "backend" {
-  name                = "${var.project_name}-appserviceplan"
-  resource_group_name = azurerm_resource_group.backend-app.name
-  location            = azurerm_resource_group.backend-app.location
-  os_type             = "Linux"
-  sku_name            = "B1"
+resource "azurerm_container_app_environment" "aca_environment" {
+  name                       = "aca-environment"
+  location                   = azurerm_resource_group.rg.location
+  resource_group_name        = azurerm_resource_group.rg.name
+  log_analytics_workspace_id = azurerm_log_analytics_workspace.workspace.id
 }
 
-resource "azurerm_linux_web_app" "backend" {
-  name                = "${var.project_name}-webapp"
-  resource_group_name = azurerm_resource_group.backend-app.name
-  location            = azurerm_service_plan.backend.location
-  service_plan_id     = azurerm_service_plan.backend.id
+resource "azurerm_container_app" "aca" {
+  name                         = "dicesaralapply07"
+  container_app_environment_id = azurerm_container_app_environment.aca_environment.id
+  resource_group_name          = azurerm_resource_group.rg.name
+  revision_mode                = "Single"
 
-  site_config {
-    always_on = "true"
-    application_stack {
-      docker_image = "thisacr.azurecr.io/imagename:latest"
-      #   docker_image     = "${azurerm_container_registry.acr.login_server}/${var.image_name}:latest"
-      docker_image_tag = "latest"
-      python_version   = "3.9"
+  template {
+    container {
+      name   = "dicefrontcontainer07"
+      image  = "thisacr.azurecr.io/imagename:latest"
+      cpu    = 0.25
+      memory = "0.5Gi"
     }
   }
 
-  app_settings = {
-    "DOCKER_REGISTRY_SERVER_URL"      = "https://thisacr.azurecr.io"
-    "DOCKER_REGISTRY_SERVER_USERNAME" = "thisacr"
-    "DOCKER_REGISTRY_SERVER_PASSWORD" = "U9+ivfherZPq3+UWDnj1fxftpOqWUgXqspIc90YYFI+ACRBkerUy"
-    # "DOCKER_REGISTRY_SERVER_USERNAME" = azurerm_container_registry.acr.admin_username
-    # "DOCKER_REGISTRY_SERVER_PASSWORD" = azurerm_container_registry.acr.admin_password
+  ingress {
+    target_port                = 80
+
+    traffic_weight {
+      latest_revision = true
+      percentage      = 100
+    }
   }
 }
+
+output "app_url" {
+  value = azurerm_container_app.aca.latest_revision_fqdn
+}
+
+
+# terraform {
+#   required_providers {
+#     azurerm = {
+#       source  = "hashicorp/azurerm"
+#       version = "=3.0.0"
+#     }
+#   }
+#   backend "azurerm" {
+#     resource_group_name  = "thisstoragerg"
+#     storage_account_name = "dicestorage02"
+#     container_name       = "13form"
+#     key                  = "tfstatedice"
+#   }
+# }
+
+# provider "azurerm" {
+#   features {}
+# }
+
+# resource "azurerm_resource_group" "backend-app" {
+#   name     = "${var.resource_group_name_prefix}-backend"
+#   location = var.resource_group_location
+# }
+# resource "azurerm_service_plan" "backend" {
+#   name                = "${var.project_name}-appserviceplan"
+#   resource_group_name = azurerm_resource_group.backend-app.name
+#   location            = azurerm_resource_group.backend-app.location
+#   os_type             = "Linux"
+#   sku_name            = "B1"
+# }
+
+# resource "azurerm_linux_web_app" "backend" {
+#   name                = "${var.project_name}-webapp"
+#   resource_group_name = azurerm_resource_group.backend-app.name
+#   location            = azurerm_service_plan.backend.location
+#   service_plan_id     = azurerm_service_plan.backend.id
+
+#   site_config {
+#     always_on = "true"
+#     application_stack {
+#       docker_image = "thisacr.azurecr.io/imagename:latest"
+#       #   docker_image     = "${azurerm_container_registry.acr.login_server}/${var.image_name}:latest"
+#       docker_image_tag = "latest"
+#       python_version   = "3.9"
+#     }
+#   }
+
+#   app_settings = {
+#     "DOCKER_REGISTRY_SERVER_URL"      = "https://thisacr.azurecr.io"
+#     "DOCKER_REGISTRY_SERVER_USERNAME" = "thisacr"
+#     "DOCKER_REGISTRY_SERVER_PASSWORD" = "U9+ivfherZPq3+UWDnj1fxftpOqWUgXqspIc90YYFI+ACRBkerUy"
+#     # "DOCKER_REGISTRY_SERVER_USERNAME" = azurerm_container_registry.acr.admin_username
+#     # "DOCKER_REGISTRY_SERVER_PASSWORD" = azurerm_container_registry.acr.admin_password
+#   }
+# }
