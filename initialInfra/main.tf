8546b2c5d2f2ff6b1a834ac7390174172a625a76
UttU28 <utsavmaan28@gmail.com>
2024-08-13 11:01:14 -0500

    sdv

--- initialInfra/main.tf
+++ initialInfra/main.tf
@@ -12,14 +12,22 @@ terraform {
   }
 }
 
-
 resource "azurerm_resource_group" "resource_group" {
   name     = local.dicesaralapply-rg
   location = local.general-location
+}
+
+resource "azurerm_log_analytics_workspace" "log_analytics_workspace" {
+  name                = local.dicesaralapply-log-analytics-workspace
+  location            = azurerm_resource_group.resource_group.location
+  resource_group_name = azurerm_resource_group.resource_group.name
+  sku                 = "PerGB2018"
+  retention_in_days   = 30
 }
 
 resource "azurerm_container_app_environment" "app_environment" {
   name                = local.dicesaralapply-app-environment
   location            = azurerm_resource_group.resource_group.location
   resource_group_name = azurerm_resource_group.resource_group.name
+  log_analytics_workspace_id = azurerm_log_analytics_workspace.log_analytics_workspace.id
 }
